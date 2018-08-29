a447aef175c839885f6a7ccda58be68b48c26b30
franc <franc@FranciPC>
2018-08-29 19:17:21 +0200

    Inizializzazione del repository Tamagotchi

---
+++ TamagotchiPersonale/src/tamagotchi/TamagotchiMain.java
@@ -0,0 +1,22 @@
+
+package tamagotchi;
+
+import it.unibs.fp.mylib.InputDati;
+import java.util.*;
+
+public class TamagotchiMain {
+
+	private static final String saluto = "Benvenuto a Tamagotchi Land";
+	private static final String crea = "Crea il tuo Tamagotchi personale, dovrai inserire il nome qui di seguito: ";
+	private static final String aCapo = "\n";
+	
+	public static void main(String[] args) {
+		
+		Tamagotchi tamagotchi;
+		System.out.println(saluto+aCapo);
+		System.out.print(crea);
+		
+		
+	}
+	
+}
